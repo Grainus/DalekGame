93eadf67b767fe0615babdc526920718bdfe341e
Phil-DB <e.pdepadovaberube@etu.cvm.qc.ca>
2022-09-24 16:55:10 -0400

    Ajoute un item {date, nom, difficulte, score} a un liste et reverse sort la list avec le score comme key

---
+++ HighScore.py
@@ -0,0 +1,18 @@
+import datetime 
+class HighScore():
+   liste_score = []
+   def get_score(liste):
+      return liste['score']
+   def get_nom(liste):
+      return liste['nom']
+   def get_diff(liste):
+      return liste['difficulte']
+   def get_date(liste):
+      return liste['date']
+   #pour que la liste aille toujours le high score en premier
+   
+def push_score(nom, difficulte,score):
+   date = str(datetime.datetime.now().year)+"/"+str(datetime.datetime.now().month)+"/"+str(datetime.datetime.now().day)
+   new_score = {'date': date, 'nom':nom, 'difficulte':difficulte,'score': score}
+   HighScore().liste_score.append(new_score)
+   HighScore().liste_score.sort(reverse = True, key = HighScore.get_score)
