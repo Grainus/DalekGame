75f2f5de12da53f3320aad2270f4e6dd6c41775b
Phil-DB <95884031+Phil-DB@users.noreply.github.com>
2022-09-24 17:46:34 -0400

    Update GameView.py

--- GameView.py
+++ GameView.py
@@ -14,21 +14,21 @@ class GameView():
         print( ("Difficulte: "+difficulte).center(int(posx*1/3)) +("Niveau: " + niveau).center(int((posx * 1/3))) + ("Score: " + score).center(int(posx*1/3)))
         print()
 
-    def afficher_les_cases(self, info_de_la_case):#info_de_la_case = grid, module a appeler a la fin d'une action valide
+   def afficher_les_cases(self, info_de_la_case, rows, col):#info_de_la_case = grid, module a appeler a la fin d'une action valide
         ligne_a_afficher = "" 
         #Boucle pour string des cases, une fois qu'on a la string il faut simplement la print(string) n fois pour faire la heuteur de la case
-        for nb_colone in range(self.rows_grid): 
-            for nb_case_par_ligne in range(self.columns_grid):
-                if (info_de_la_case[nb_colone][nb_case_par_ligne] == 'D'):
+        for nb_rows in range(col): 
+            for nb_col in range(rows):
+                if (info_de_la_case[nb_rows][nb_col] == 'D'):
                     ligne_a_afficher += (("X"*self.largeur_case)+(" "*self.espace_entre_cases))
 
-                elif (info_de_la_case[nb_colone][nb_case_par_ligne] == 'J'):
+                elif (info_de_la_case[nb_rows][nb_col] == 'J'):
                     ligne_a_afficher += (("F"*self.largeur_case)+(" "*self.espace_entre_cases))
 
-                elif (info_de_la_case[nb_colone][nb_case_par_ligne] == 'A'):
+                elif (info_de_la_case[nb_rows][nb_col] == 'A'):
                     ligne_a_afficher += (("D"*self.largeur_case)+(" "*self.espace_entre_cases))
 
-                elif (info_de_la_case[nb_colone][nb_case_par_ligne] == ' '):
+                elif (info_de_la_case[nb_rows][nb_col] == ' '):
                     ligne_a_afficher += (("C"*self.largeur_case)+(" "*self.espace_entre_cases))
                 else:
                     None
@@ -36,7 +36,7 @@ class GameView():
                 #Ne pas oublier d'enlever le "  " a la fin des strings quand on affiche le jeu bien centrer ex: "CCCCC  CCCCC  " -> "CCCCC  CCCCC"
                 print(ligne_a_afficher.center(get_terminal_size().columns - self.espace_entre_cases))
             print()
-            ligne_a_afficher = "" #Ensuite la remettre a "" pour la deuxieme rangee de cases et ainsi de suite    
+            ligne_a_afficher = "" #Ensuite la remettre a "" pour la deuxieme rangee de cases et ainsi de suite        
 
     def afficher_footer(self, sorte_de_teleporteur, nb_de_zap):    
         posx = get_terminal_size().columns 
