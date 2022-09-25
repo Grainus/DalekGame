1a93d97e5416f80dc26424299aa35e206296b89e
Phil-DB <95884031+Phil-DB@users.noreply.github.com>
2022-09-25 17:14:48 -0400

    Update MenuView.py

--- MenuView.py
+++ MenuView.py
@@ -1,10 +1,9 @@
-from os import get_terminal_size
+from os import get_terminal_size, system
 #peut etre faire de menuView et highScoreView des sous classes de GameView
  
 class MenuView:
-
-   def afficher_menu(self):
-
+   def afficher_menu():
+      system('cls')
       print("\n"+("DALEK\n").center(get_terminal_size().columns)+"\n"+
       ("Choisissez vos options...").center(get_terminal_size().columns)+"\n"+
       " "*int(get_terminal_size().columns*1/6)+"Difficulte  1. Facile\n"+ 
@@ -12,7 +11,4 @@ class MenuView:
       " "*int(get_terminal_size().columns*1/6+len("Difficulte  "))+"3. Difficile\n\n"+
       " "*int(get_terminal_size().columns*1/6)+"Mode de debug: (Fin ou 01) \n\n"+
       ("C = Case  D = Docteur X = Dalek   F = Ferraille").center(get_terminal_size().columns)+"\n\n"+
-      ("APPUYEZ SUR ENTRE POUR CONTINUER...").center(get_terminal_size().columns)
-      )
-
-# catch EventManager debug O/N, difficultee: facile medium difficile
+      ("APPUYEZ SUR ENTRE POUR CONTINUER...").center(get_terminal_size().columns)+"\n")
