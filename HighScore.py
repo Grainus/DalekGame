bca5457d3517874822b843cc4a99122d2faf7470
Phil-DB <95884031+Phil-DB@users.noreply.github.com>
2022-09-25 17:13:30 -0400

    Update HighScore.py

--- HighScore.py
+++ HighScore.py
@@ -1,8 +1,12 @@
 import datetime 
-import Game
+from game import Game
+#import Game
 
 class HighScore():
-   liste_score = []
+   def __init__(self, game: Game):
+      self.game = game
+      self.liste_score = []
+   
    def get_score(liste):
       return liste['score']
    def get_nom(liste):
@@ -11,13 +15,23 @@ class HighScore():
       return liste['difficulte']
    def get_date(liste):
       return liste['date']
-   #pour que la liste aille toujours le high score en premier
    
-def push_score():
-   difficulte = Game.Game().difficulte
-   nom = Game.Game().nom
-   score = Game.Game().score
-   date = str(datetime.datetime.now().year)+"/"+str(datetime.datetime.now().month)+"/"+str(datetime.datetime.now().day)
-   new_score = {'date': date, 'nom':nom, 'difficulte':difficulte,'score': score}
-   HighScore().liste_score.append(new_score)
-   HighScore().liste_score.sort(reverse = True, key = HighScore.get_score)
+   def demander_nom():
+      print()
+      nom = input("Nom: ")
+      return nom   
+
+   #pour que la liste aille toujours le high score en premier
+   def push_score(self):
+      if ( self.game.difficulty == 0):
+         difficulte = "Facile"
+      if ( self.game.difficulty == 1):
+         difficulte = "Moyen"
+      if ( self.game.difficulty == 2):
+         difficulte = "Difficile"
+      nom = self.demander_nom()
+      score = self.game.score
+      date = str(datetime.datetime.now().year)+"/"+str(datetime.datetime.now().month)+"/"+str(datetime.datetime.now().day)
+      new_score = {'date': date, 'nom':nom, 'difficulte':difficulte,'score': score}
+      self.liste_score.append(new_score)
+      self.liste_score.sort(reverse = True, key = self.get_score)
