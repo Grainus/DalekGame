ba8d64b51875041607cb2f9d97185601eeed83a5
Phil-DB <95884031+Phil-DB@users.noreply.github.com>
2022-09-25 17:14:06 -0400

    Update HighScoreView.py

--- HighScoreView.py
+++ HighScoreView.py
@@ -1,22 +1,31 @@
-import HighScore
+from highscore import HighScore
 from os import get_terminal_size# Get size of terminal
 from os import system, name
 
 class HighScoreView(object):
 
+    def __init__(self, highscore : HighScore):
+        self.highscore = highscore
+
     def afficher_header(self):
         posx = get_terminal_size().columns 
-        print( (("Date").center(int(posx*1/8)) + ("Nom").center(int(posx*1/8))+("Difficulte").center(int(posx*1/8) ) + ("Score").center(int(posx*1/8))).center(posx))
+        print( "\n"+(("Date").center(int(posx*1/8)) + ("Nom").center(int(posx*1/8))+("Difficulte").center(int(posx*1/8) ) + ("Score").center(int(posx*1/8))).center(posx))
 
-    def afficher_score(self, date, nom, difficulte, score):
+    def afficher_score(self,date,nom,dif,score):
+        date = self.highscore.date 
+        nom = self.highscore.nom 
+        dif = self.highscore.difficulte 
+        score = self.highscore.score
         posx = get_terminal_size().columns 
-        print((date.center(int(posx*1/8)) + nom.center(int(posx*1/8))+difficulte.center(int(posx*1/8)) + score.center(int(posx*1/8))).center(posx))
+        print((date.center(int(posx*1/8)) + nom.center(int(posx*1/8))+dif.center(int(posx*1/8)) + score.center(int(posx*1/8))).center(posx))
 
-def afficher_tableau_score():
-    HighScoreView().afficher_header()
-    for i in range(len(HighScore.HighScore().liste_score)):
-        score = str(HighScore.HighScore.get_score(HighScore.HighScore().liste_score[i]))
-        nom =  HighScore.HighScore.get_nom(HighScore.HighScore().liste_score[i])
-        dif =  HighScore.HighScore.get_diff(HighScore.HighScore().liste_score[i])
-        date = HighScore.HighScore.get_date(HighScore.HighScore().liste_score[i])
-        HighScoreView().afficher_score(date, nom, dif, score)
+    def afficher_tableau_score(self):
+        system('cls')
+        self.afficher_header()
+        for i in range(len(self.highscore.liste_score)):
+            score = str(self.highscore.get_score(self.highscore.liste_score[i]))
+            nom =  self.highscore.get_nom(self.highscore.liste_score[i])
+            dif =  self.highscore.get_diff(self.highscore.liste_score[i])
+            date = self.highscore.get_date(self.highscore.liste_score[i])
+            self.afficher_score(date, nom, dif, score)
+        print('\n')
