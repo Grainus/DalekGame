52724416ad575cf1c208fdf7e1ae9afa5e19e3cf
Grainus <98911989+Grainus@users.noreply.github.com>
2022-09-26 12:33:49 -0400

    Make menuview comply with event manager

--- MenuView.py
+++ MenuView.py
@@ -1,14 +1,24 @@
 from os import get_terminal_size, system
 #peut etre faire de menuView et highScoreView des sous classes de GameView
+
+from eventmanager import EventListener, Event, DrawEvent
+from models import State
  
-class MenuView:
-   def afficher_menu():
-      system('cls')
-      print("\n"+("DALEK\n").center(get_terminal_size().columns)+"\n"+
-      ("Choisissez vos options...").center(get_terminal_size().columns)+"\n"+
-      " "*int(get_terminal_size().columns*1/6)+"Difficulte  1. Facile\n"+ 
-      " "*int(get_terminal_size().columns*1/6+len("Difficulte  "))+"2: Moyen\n"+
-      " "*int(get_terminal_size().columns*1/6+len("Difficulte  "))+"3. Difficile\n\n"+
-      " "*int(get_terminal_size().columns*1/6)+"Mode de debug: (Fin ou 01) \n\n"+
-      ("C = Case  D = Docteur X = Dalek   F = Ferraille").center(get_terminal_size().columns)+"\n\n"+
-      ("APPUYEZ SUR ENTRE POUR CONTINUER...").center(get_terminal_size().columns)+"\n")
+class MenuView(EventListener):
+    def notify(self, event: Event):
+        if isinstance(event, DrawEvent):
+            if event.state == State.MENU:
+                MenuView.afficher_menu()
+                
+    @staticmethod
+    def afficher_menu():
+        system('cls')
+        termsize = get_terminal_size().columns
+        print("\n"+("DALEK GAME\n").center(termsize)+"\n"+
+        ("Choose your option...").center(termsize)+"\n"+
+        " "*int(termsize*1/6)+"Difficulty  1. Easy\n"+ 
+        " "*int(termsize*1/6+len("Difficulty  "))+"2: Medium\n"+
+        " "*int(termsize*1/6+len("Difficulty  "))+"3. Hard\n\n"+
+        " "*int(termsize*1/6)+"Debug mode: (Fin ou 01) \n\n"+
+        ("C = Case  D = Doctor  X = Dalek  J = Junk").center(termsize)+"\n\n"+
+        ("PRESS ENTER TO CONTINUE...").center(termsize)+"\n")
