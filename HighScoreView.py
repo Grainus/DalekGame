c4c51c532aeb816db35eb7f488578172f6c31d55
Grainus <98911989+Grainus@users.noreply.github.com>
2022-09-26 12:00:59 -0400

    Adapt highscoreview to sql

--- HighScoreView.py
+++ HighScoreView.py
@@ -1,31 +1,38 @@
+from os import get_terminal_size, system
+
 from highscore import HighScore
-from os import get_terminal_size# Get size of terminal
-from os import system, name
+from eventmanager import EventManager, EventListener, Event, DrawEvent
+from models import State
 
-class HighScoreView(object):
-
-    def __init__(self, highscore : HighScore):
-        self.highscore = highscore
-
-    def afficher_header(self):
+class HighScoreView(EventListener):
+    
+    def notify(self, event: Event):
+        if isinstance(event, DrawEvent):
+            if event.state == State.HIGHSCORE:
+                HighScoreView.afficher_tableau_score()
+    
+    @staticmethod
+    def afficher_header() -> None:
         posx = get_terminal_size().columns 
         print( "\n"+(("Date").center(int(posx*1/8)) + ("Nom").center(int(posx*1/8))+("Difficulte").center(int(posx*1/8) ) + ("Score").center(int(posx*1/8))).center(posx))
 
-    def afficher_score(self,date,nom,dif,score):
-        date = self.highscore.date 
-        nom = self.highscore.nom 
-        dif = self.highscore.difficulte 
-        score = self.highscore.score
+    @staticmethod
+    def afficher_score(scoretuple) -> None:
+        dif, name, score, date = scoretuple
         posx = get_terminal_size().columns 
-        print((date.center(int(posx*1/8)) + nom.center(int(posx*1/8))+dif.center(int(posx*1/8)) + score.center(int(posx*1/8))).center(posx))
+        print(
+            (
+                date.center(int(posx*1/8))
+                + name.center(int(posx*1/8))
+                + dif.center(int(posx*1/8))
+                + score.center(int(posx*1/8))
+            ).center(posx)
+        )
 
-    def afficher_tableau_score(self):
+    @staticmethod
+    def afficher_tableau_score() -> None:
         system('cls')
-        self.afficher_header()
-        for i in range(len(self.highscore.liste_score)):
-            score = str(self.highscore.get_score(self.highscore.liste_score[i]))
-            nom =  self.highscore.get_nom(self.highscore.liste_score[i])
-            dif =  self.highscore.get_diff(self.highscore.liste_score[i])
-            date = self.highscore.get_date(self.highscore.liste_score[i])
-            self.afficher_score(date, nom, dif, score)
+        HighScoreView.afficher_header()
+        for score in HighScore.get_scores():
+            HighScoreView.afficher_score(score)
         print('\n')
